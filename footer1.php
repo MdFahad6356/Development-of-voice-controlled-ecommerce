f75d664709cbcccb1abc876fc817d5def6989ecc
MdFahad6356 <mdfahad6356@gmail.com>
2021-10-05 23:48:10 +0600

    initial commit

---
+++ footer1.php
@@ -0,0 +1,145 @@
+<head>
+		<title>Voice Commerce</title>
+		<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+  *,*: before,*after{
+    box sizing: border-box;
+}
+body{
+    font-family: poppins;
+    margin: 0;
+    display grid;
+    font-size: 14px;0
+}
+
+footer{
+    display: -webkit-flex;
+    display: -moz-flex;
+    display: -ms-flex;
+    display: -o-flex;
+    display: flex;
+    flex-flow: row wrap;
+    padding: 50px;
+    color: #fff;
+    background-color: #011c39;
+}
+.footer > *{
+    flex: 1 100%;
+}
+.footer-left{
+    margin-right: 1.25em;
+    margin-bottom: 2em;
+}
+.footer-left img{
+    width: 8%;
+}
+h2{
+    font-weight:600;
+    font-size: 17px;
+}
+.footer ul{
+    list-style: none;
+    padding-left: 0;
+}
+.footer li{
+    line-height: 2em;
+}
+.footer a{
+    text-decoration: none;
+}
+.footer-right{
+    display: -webkit-flex;
+    display: -moz-flex;
+    display: -ms-flex;
+    display: -o-flex;
+    display: flex;
+    display: row wrap;
+}
+.footer-right > *{
+    flex: 1 50%;
+}
+.box a{
+    color:#999;
+}
+.footer-bottom{
+    text-align: center;
+    color:#999;
+    padding-top: 50px;
+}
+.footer-left p{
+    padding-top: 50px;
+    color: #999;
+}
+
+@media screen and (min-width: 600px){
+    .footer-right> *{
+        flex:1;
+    }
+    .footer-left{
+        flex: 1 0px;
+    }
+    .footer-right{
+        flex: 2 0px;
+    }
+}
+
+@media (max-width: 600px){
+        .footer{
+            padding:15px;
+        }
+        main{
+            font-size: 55px;
+        }
+    }
+</style>
+		
+	<link rel="stylesheet" href="styles/style1.css" media="all" /> 
+	
+	</head>
+	
+<body>
+<footer class = "footer">
+                <div class = "footer-left">
+                    <img src = "pic.jpg" alt = "">
+                    <p> Find us at</p>
+                    <div class = "socials">
+                        <a href = "#"><img src = "fb.jpg" alt = "" ><i class ="fa-facebook" ></i></a>
+                        <a href = "#"><img src = "youtube.jpg" alt = "" width = "" height = ""><i class ="fa-youtube" ></i></a>
+                        <a href = "#"><img src = "twitter.jpg" alt = "" width = "" height = ""><i class ="fa-twitter" ></i></a>
+                        
+                    </div>
+                </div>
+                    <ul class = "footer-right">
+                        <li>
+                            <h2> Product</h2>
+                            <ul class = "box">
+                                <li><a href = "#">Men's</a></li>
+                                <li><a href = "#">Women's</a></li>
+                                <li><a href = "#">Musical Instruments</a></li>
+                                <li><a href = "#">Fitness</a></li> 
+                            </ul>
+                        </li>
+                        <li>
+                            <h2>Necessary Links</h2>
+                            <ul class = "box">
+                                <li><a href = "#">Pricing</a></li>
+                                <li><a href = "#">Sales</a></li>
+                                <li><a href = "#">Customer Services</a></li>
+                                <li><a href = "#">Certifications</a></li> 
+                            </ul>
+                        </li>
+                        <li>
+                            <h2> Address</h2>
+                            <ul class = "box">
+                                <li><a href = "#">7/5 Salimullah Road</a></li>
+                                <li><a href = "#">Mdpur, Dhaka-1207</a></li>
+                                <li><a href = "#">Bangladesh</a></li>
+                                 
+                            </ul>
+                        </li>
+                    </ul>
+            </footer>
+	
+	
+		</body>
